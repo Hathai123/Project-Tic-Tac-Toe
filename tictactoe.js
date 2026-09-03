8e8c8e2c1facc0a3d97e384909d11c392c2f70af
Hathai123 <126381627+Hathai123@users.noreply.github.com>
2026-09-03 17:15:52 +0700

    set base pseudo code

---
+++ tictactoe.js
@@ -0,0 +1,18 @@
+// tictactor : OX game
+// test first in console 
+// Game board show in console
+// [1][2][3]
+// [4][5][6]
+// [7][8][9]
+
+// Select the position by type select(5)
+// Console will show
+// [1][2][3]
+// [4][X][6]
+// [7][8][9]
+
+// Winning condition
+// Who’s get one of these set of position first win
+// 123, 456, 789, 147, 258, 369, 159, 357
+
+// Game start with X and then O
