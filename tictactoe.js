80127cb8cda69cd6ca98dd7121e59f1264b0a0c5
Hathai123 <126381627+Hathai123@users.noreply.github.com>
2026-09-04 15:06:44 +0700

    set win condition + fix newGame

--- tictactoe.js
+++ tictactoe.js
@@ -19,15 +19,19 @@
 const game = (() => {
     const x_pos = [];
     const o_pos = [];
-    const board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     let whoTurn = "X";
+    let isWin = false;
 
     const newGame = () => {
-        x_player = [];
-        o_player = [];
+        x_pos.length = 0;
+        o_pos.length = 0;
         board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
         whoTurn = "X";
+        isWin = false;
+        showBoard();
     };
+
     const showBoard = () => {
         let gameBoard = board.reduce((result, item, index) => {
             const display = `[${item}]`;
@@ -41,25 +45,34 @@ const game = (() => {
         board[board.indexOf(pos)] = whoTurn;
         if (whoTurn === "X") {
             x_pos.push(pos);
-            checkWin(x_pos)
+            checkWin(x_pos);
             whoTurn = "O";
         }
         else {
             o_pos.push(pos);
-            checkWin(o_pos)
+            checkWin(o_pos);
             whoTurn = "X";
         }
-        showBoard();
     };
 
     function checkWin(player_pos) {
-
+        const winCondition = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+        for (let con of winCondition) {
+            if (con.every((element) => player_pos.includes(element))) {
+                isWin = true;
+                break;
+            }
+        }
+        showBoard();
+        if (isWin) {
+            console.log(`${whoTurn} WIN!!!`);
+        }
 
     }
 
 
 
-    return { newGame, showBoard, select};
+    return { newGame, showBoard, select };
 })();
 
 game.showBoard();
