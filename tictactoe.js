ab553d871a81c709b0eecdc42df10c103663730d
Hathai123 <126381627+Hathai123@users.noreply.github.com>
2026-09-04 14:32:11 +0700

    can show board game in console

--- tictactoe.js
+++ tictactoe.js
@@ -1,11 +1,11 @@
-// tictactor : OX game
+// tictactoe : OX game
 // test first in console 
 // Game board show in console
 // [1][2][3]
 // [4][5][6]
 // [7][8][9]
 
-// Select the position by type select(5)
+// Select the position by type game.select(5) in console
 // Console will show
 // [1][2][3]
 // [4][X][6]
@@ -16,3 +16,32 @@
 // 123, 456, 789, 147, 258, 369, 159, 357
 
 // Game start with X and then O
+const game = (() => {
+    const x_pos = [];
+    const o_pos = [];
+    const board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let whoTurn = "X";
+
+    const newGame = () => {
+        x_player = [];
+        o_player = [];
+        board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        whoTurn = "X";
+    };
+    const showBoard = () => {
+        let gameBoard = board.reduce((result, item, index) => {
+            const display = `[${item}]`;
+            const separator = (index + 1) % 3 === 0 ? '\n' : ' ';
+            return result + display + separator;
+        }, "");
+        console.log(gameBoard);
+    };
+
+    
+
+
+
+    return { newGame, showBoard, };
+})();
+
+game.showBoard();
